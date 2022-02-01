6d499dcfd9b119e7c31160710924e6df3f58991c
Umar Farouk Yusuf <umaryusuf11@gmail.com>
2022-02-01 00:01:14 +0000

    + Chapter 6

--- populate_rango.py
+++ populate_rango.py
@@ -9,26 +9,50 @@ from rango.models import Category, Page
 
 def populate():
     python_pages = [
-        {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
-        {'title':'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
-        {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'} ]
+        {
+         'title': 'Official Python Tutorial',
+         'url':'http://docs.python.org/3/tutorial/',
+         'views':110,
+        },
+        {
+         'title':'How to Think like a Computer Scientist',
+         'url':'http://www.greenteapress.com/thinkpython/',
+         'views':55,
+         },
+        {
+         'title':'Learn Python in 10 Minutes',
+         'url':'http://www.korokithakis.net/tutorials/python/',
+         'views': 21,
+         }, ]
     
     django_pages = [
-        {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
-        {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'} ]
+        {
+         'title':'Official Django Tutorial',
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/',
+         'views': 88,
+         },
+        {
+         'title':'Django Rocks',
+         'url':'http://www.djangorocks.com/',
+         'views': 73,
+        },
+        {
+         'title':'How to Tango with Django',
+         'url':'http://www.tangowithdjango.com/',
+         'views': 56,
+         } ]
     
     other_pages = [
-        {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
-        {'title':'Flask',
-         'url':'http://flask.pocoo.org'} ]
+        {
+         'title':'Bottle',
+         'url':'http://bottlepy.org/docs/dev/',
+         'views': 36,
+        },
+        {
+         'title':'Flask',
+         'url':'http://flask.pocoo.org',
+         'views': 16,
+        } ]
     
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
             'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
@@ -37,7 +61,7 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
     
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
--- rango/admin.py
+++ rango/admin.py
@@ -5,6 +5,10 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
